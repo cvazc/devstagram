196a36ba6ac58be3c652d37801c3cfb840bca953
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2024-12-07 01:00:56 -0600

    Add registration view and update routing for user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
             <nav class="flex gap-2 items-center">
                 <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                <a class="font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                <a class="font-bold uppercase text-gray-600 text-sm" href="/register">Crear Cuenta</a>
             </nav>
         </div>
     </header>
