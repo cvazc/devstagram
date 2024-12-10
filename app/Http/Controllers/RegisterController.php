98d250cc48773ec5c0ffa54145cf00e8b86c4f04
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2024-12-09 22:28:58 -0600

    Add username handling to registration and implement post feed route

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,7 +16,7 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        // Validation
+        $request->request->add(['username' => Str::slug($request->username)]);
         $request->validate([
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
@@ -21,6 +24,13 @@ class RegisterController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
-        //dd('Creando usuario');
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('posts.index');
     }
 }
