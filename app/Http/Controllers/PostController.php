bd60afe82787a18136ef408175563c196004151a
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2024-12-13 06:58:06 -0600

    Add user dashboard view and enforce authentication on post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index ()
     {
-        dd(Auth::user());
+        return view('dashboard');
     }
 }
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title')
+    Tu Cuenta
+@endsection
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
+            <div class="md:w-8/12 lg:w-6/12 px-5">
+                <img src="{{ Vite::asset('/public/img/user.svg') }}" alt="Image user">
+            </div>
+            <div class="md:w-8/12 lg:w-6/12 px-5">
+                <p class="text-gray-700 text-2xl">{{ auth()->user()->username }}</p>
+            </div>
+        </div>
+    </div>
+@endsection
